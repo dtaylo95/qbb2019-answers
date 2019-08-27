480ac03df0c5cf33177b63e59328ffad6c1858c0
Dylan Taylor <dtaylo95@jhu.edu>
2019-08-26 20:52:40 -0400

    Finished advanced questions for homework

---
+++ day1-homework/parse_sam_extra.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env Python3
+
+import sys
+import re
+
+def parse_CIGAR(c_string):
+	numerals = '123456789'
+	parsed_CIGAR = []
+	count = ''
+	identity = ''
+	for char in c_string:
+		if char in numerals:
+			count += char
+		else:
+			identity = char
+			parsed_CIGAR.append((count, identity))
+			count = ''
+			identity = ''
+	return parsed_CIGAR
+
+
+if len(sys.argv) > 1:
+	f = open(sys.argv[1])
+else:
+	f = sys.stdin
+
+reverse_comp_count = 0
+bit_mask = 0b10000
+over30score_count = 0
+indel1 = 0
+indel2 = 0
+indel3 = 0
+indel4 = 0
+indelbig = 0
+
+for line in f:
+	fields = line.rstrip('\n').split('\t')
+	flag = int(fields[1])
+	if flag & bit_mask:
+		reverse_comp_count += 1
+	
+	qual_score = fields[10]
+	chars = 0
+	total_scores = 0
+	for char in qual_score:
+		chars += 1
+		total_scores += ord(char) - 33
+	avg_score = float(total_scores)/chars
+	if avg_score > 30:
+		over30score_count += 1
+
+	CIGAR_string = fields[5]
+	parsed_CIGAR = re.findall(r"[^\W\d_]+|\d+", CIGAR_string)
+	if 'I' in parsed_CIGAR:
+		indel_score = int(parsed_CIGAR[parsed_CIGAR.index('I') - 1])
+		if indel_score == 1:
+			indel1 += 1
+		elif indel_score == 2:
+			indel2 += 1
+		elif indel_score == 3:
+			indel3 += 1
+		elif indel_score == 4:
+			indel4 += 1
+		elif indel_score > 4:
+			indelbig += 1
+	if 'D' in parsed_CIGAR:
+		indel_score = int(parsed_CIGAR[parsed_CIGAR.index('D') - 1])
+		if indel_score == 1:
+			indel1 += 1
+		elif indel_score == 2:
+			indel2 += 1
+		elif indel_score == 3:
+			indel3 += 1
+		elif indel_score == 4:
+			indel4 += 1
+		elif indel_score > 4:
+			indelbig += 1		
+
+
+print(reverse_comp_count)
+print(over30score_count)
+print(indel1, indel2, indel3, indel4, indelbig)
