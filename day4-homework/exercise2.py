4ceddc5f9a91c008d13a8cc4223dd73b474cb908
Dylan Taylor <dtaylo95@jhu.edu>
2019-08-30 09:39:52 -0400

    Updated exercise2 and corresponding boxplot to scale y-axis uniformly

--- day4-homework/exercise2.py
+++ day4-homework/exercise2.py
@@ -27,6 +27,9 @@ male_transcripts = np.log2(filtered_fpkms[male_ids] + 0.001)
 female_ids = [x for x in list(filtered_fpkms) if x.startswith('female')]
 female_transcripts = np.log2(filtered_fpkms[female_ids] + 0.001)
 
+min_y = np.floor(min(male_transcripts.values.min(), female_transcripts.values.min()))
+max_y = np.ceil(max(male_transcripts.values.max(), female_transcripts.values.max()))
+
 male_labels = [y[5:] for y in male_ids]
 female_labels = [x[7:] for x in female_ids]
 
@@ -36,11 +39,13 @@ ax1.set_xticklabels(male_labels, fontsize=7)
 ax1.set_title('Male Sxl Time Series')
 ax1.set_xlabel('Developmental Stage')
 ax1.set_ylabel('log2(FPKM) value of transcripts')
+ax1.set_ylim(min_y, max_y)
 ax2.boxplot(female_transcripts.T)
 ax2.set_xticklabels(female_labels, fontsize=7)
 ax2.set_title('Female Sxl Time Series')
 ax2.set_xlabel('Developmental Stage')
 ax2.set_ylabel('log2(FPKM) value of transcripts')
+ax2.set_ylim(min_y, max_y)
 plt.tight_layout()
 fig.savefig('boxplot.png')
 plt.close()
